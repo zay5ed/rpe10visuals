6ae566996c4ded96b01ccb3634549ed4a09577fe
zay5ed <zayedahmed1105@gmail.com>
2026-03-04 00:28:56 +0530

    trying to fix navbar overlap

--- rpe10visuals/app/page.tsx
+++ rpe10visuals/app/page.tsx
@@ -15,7 +15,10 @@ export default function Home() {
     <main className="min-h-screen relative overflow-x-hidden touch-pan-y">
       <div className="relative overflow-x-hidden">
         <Navbar />
-        <div className="h-[calc(env(safe-area-inset-top,0px)+96px)] md:h-[calc(env(safe-area-inset-top,0px)+108px)]" aria-hidden="true" />
+        <div
+          className="h-[calc(env(safe-area-inset-top,0px)+132px)] md:h-[calc(env(safe-area-inset-top,0px)+148px)]"
+          aria-hidden="true"
+        />
         <HeroSlider />
         <PackagesSection />
         <AboutSection />
